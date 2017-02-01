27e54b1b680501e17df508fbfb05166fd6936eeb
Anders Thomsen <ande605x@edu.easj.dk>
2017-02-01 10:50:48 +0100

    Opgave 3.1 done

---
+++ EventMaker/Persistency/PersistencyService.cs
@@ -0,0 +1,57 @@
+﻿using EventMaker.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Windows.Storage;
+
+namespace EventMaker.Persistency
+{
+    class PersistencyService
+    {
+        static private readonly string filename = "EventFile.json";
+        static StorageFolder localfolder = null;
+
+        public static async void SaveEventAsJsonAsync(ObservableCollection<Event> events)
+        {
+            string jsonText = JsonConvert.SerializeObject(events);
+            SerializeEventsFileAsync(jsonText, filename);
+        }
+
+        public static async Task<List<Event>> LoadEventsFromJsonAsync()
+        {
+            Model.EventCatalogSingleton.Instance.EventListe.Clear();
+
+            Task<string> jsonTaskText = DeSerializeEventsFileAsync(filename);
+
+            List<Event> newList = JsonConvert.DeserializeObject<List<Event>>("hej");//jsonTaskText);
+            foreach (var i in newList)
+            {
+                newList.Add(i);
+            }
+            //Model.EventCatalogSingleton.Instance.EventListe
+            return newList;
+        }
+
+
+        private static async void SerializeEventsFileAsync(string eventsString, string filename)
+        {
+           StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+           await FileIO.WriteTextAsync(file, eventsString);
+        }
+
+
+        private static async Task<string> DeSerializeEventsFileAsync(string filename)
+        {
+            StorageFile file = await localfolder.GetFileAsync(filename);
+            string jsonText = await FileIO.ReadTextAsync(file);
+
+            
+            return jsonText;
+        }
+
+    }
+}
